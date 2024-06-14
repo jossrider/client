2f9830f8d44b114880c0f37a2d8f0d02ce574334
Joss Rider <jossrider@outlook.com>
2024-06-14 11:00:11 -0400

    Culminamos el frontend

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,6 +1,7 @@
-import { Link, Form, useActionData, ActionFunctionArgs } from 'react-router-dom'
+import { Link, Form, useActionData, ActionFunctionArgs, redirect } from 'react-router-dom'
 import ErrorMessage from '../components/ErrorMessage'
 import { addProduct } from '../services/ProductService'
+import ProductForm from '../components/ProductForm'
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData())
@@ -10,12 +11,14 @@ export async function action({ request }: ActionFunctionArgs) {
   if (Object.values(data).includes('')) {
     error = 'Todos los campos son obligatorios'
   }
+
   if (error.length) {
     return error
   }
-  addProduct(data)
 
-  return {}
+  await addProduct(data)
+
+  return redirect('/')
 }
 
 export default function NewProduct() {
@@ -25,38 +28,13 @@ export default function NewProduct() {
     <>
       <div className='flex justify-between'>
         <h2 className='text-4xl font-black text-slate-500'>Registrar Producto</h2>
-        <Link
-          to={'/'}
-          className='rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500'>
+        <Link to={'/'} className='rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500'>
           Volver a Productos
         </Link>
       </div>
       {error && <ErrorMessage>{error}</ErrorMessage>}
       <Form className='mt-10' method='POST'>
-        <div className='mb-4'>
-          <label className='text-gray-800' htmlFor='name'>
-            Nombre Producto:
-          </label>
-          <input
-            id='name'
-            type='text'
-            className='mt-2 block w-full p-3 bg-gray-50'
-            placeholder='Nombre del Producto'
-            name='name'
-          />
-        </div>
-        <div className='mb-4'>
-          <label className='text-gray-800' htmlFor='price'>
-            Precio:
-          </label>
-          <input
-            id='price'
-            type='number'
-            className='mt-2 block w-full p-3 bg-gray-50'
-            placeholder='Precio Producto. ej. 200, 300'
-            name='price'
-          />
-        </div>
+        <ProductForm />
         <input
           type='submit'
           className='mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded'
